6fa1903fa9352ae7f6b921a905d081a931f5680d
gayashan <gayashan.randimagamage@gmail.com>
2024-04-18 06:22:46 +0530

    set layout for entire webapp

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <template>
-  <h1>Gayashan.dev</h1>
+  <div class="container">
+    <div class="intro sub-container">Introduction</div>
+    <div class="menu sub-container">Menu</div>
+    <div class="content sub-container">Content</div>
+    <div class="footer sub-container">Footer</div>
+  </div>
 </template>
 
 <script setup>
 name: "App";
 </script>
+
+<style>
+* {
+  margin: 0px;
+}
+.container {
+  display: grid;
+  width: 100%;
+  height: 731px;
+  grid-template-columns: repeat (2, 1fr);
+  grid-template-rows: 2fr 20fr 2fr;
+  grid-template-areas:
+    "intoduction menu"
+    "intoduction content"
+    "footer footer";
+}
+.intro {
+  grid-area: intoduction;
+}
+.menu {
+  grid-area: menu;
+}
+.content {
+  grid-area: content;
+}
+.footer {
+  grid-area: footer;
+}
+.sub-container {
+  border: 1px solid black;
+}
+</style>
