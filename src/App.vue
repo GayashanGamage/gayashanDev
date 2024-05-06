f5223490a93b47057b820387784f8e949741b938
gayashan <gayashan.randimagamage@gmail.com>
2024-05-06 06:55:07 +0530

    restructure site from scratch:

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
-<template></template>
+<template>
+  <div class="main-container">
+    <div class="sub-containers">
+      <introduction></introduction>
+    </div>
+    <!-- <div class="sub-containers"></div>
+    <div class="sub-containers"></div> -->
+  </div>
+</template>
 
 <script setup>
+import Introduction from "./components/Introduction.vue";
 name: "App";
 </script>
 
-<style></style>
+<style>
+* {
+  margin: auto 0px;
+  font-family: "Poppins", sans-serif;
+}
+
+.main-container {
+  background-color: burlywood;
+  width: auto;
+  height: 800px;
+  padding-top: 1px;
+}
+.sub-containers {
+  /* background-color: cornflowerblue; */
+  /* border: 1px solid black; */
+  width: 60%;
+  height: 400px;
+  margin: 160px auto;
+}
+</style>
---
+++ src/components/Introduction.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <p class="intro-description">Hi... I'm...</p>
+    <h1 class="intro-name">Gayashan Gamage</h1>
+    <p class="intro-title">SOFTWARE ENGINEER</p>
+    <p class="intro-description">
+      I love build logical stuff using code and my fingers to get easier your
+      businees tasks and daily life
+    </p>
+    <div class="intro-button-div">
+      <button class="intro-button">
+        <span class="material-symbols-outlined"> description </span>Download CV
+      </button>
+      <button class="intro-button intro-button-linkedin">
+        <span class="material-symbols-outlined">person_add</span>LinkedIn
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scope>
+.intro-button-div {
+  margin-top: 5%;
+  display: flex;
+  /* justify-content: center; */
+}
+.intro-button {
+  padding: 10px 20px;
+  background-color: black;
+  color: burlywood;
+  border: 0px;
+  border-radius: 4px;
+  font-weight: 600;
+  font-size: 16px;
+}
+.intro-button-linkedin {
+  margin-left: 20px;
+}
+.intro-name {
+  font-size: 90px;
+  padding: 0 0 20px 0;
+}
+.intro-description {
+  font-size: 24px;
+  font-weight: 300;
+}
+.intro-title {
+  font-size: 24px;
+  font-weight: 900;
+  padding: 0 0 20px 0;
+}
+.material-symbols-outlined {
+  font-family: "Material Icons";
+  margin-right: 15px;
+}
+</style>
