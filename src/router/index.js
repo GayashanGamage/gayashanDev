78dd51e70a1d0b800ace21d319c223bd88202261
gayashan <gayashan.randimagamage@gmail.com>
2024-05-10 10:16:04 +0530

    correct the router link

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "home",
+      component: Mywork,
+    },
+    {
       path: "/project",
       name: "project",
       component: Mywork,
